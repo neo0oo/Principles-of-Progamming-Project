0b6d234022b070bc4b537bea334fa6fae5bf24fd
Neo Medic <neomedic926@gmail.com>
2025-05-09 14:02:14 +0200

    Update hello.c

--- hello.c
+++ hello.c
@@ -4,7 +4,8 @@ int starting_room()
 {
     char option_1;
     
-    printf("After Days of journeying through the forest you stumble upon a cave");
+    printf("You are an adventurer in a forest, clothes ripped, tired but determined.");
+    printf("After Days of journeying through the forest you stumble upon the cave of fortune.");
     printf("Will you enter?");
     scanf("%c", &option_1);
 }
