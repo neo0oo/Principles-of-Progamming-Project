5ff635e2dd1a10e95fc9d35c032d373419cca08e
Neo Medic <neomedic926@gmail.com>
2025-05-11 22:29:43 +0200

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 
+char* getItem(int index) {
+    static char* inventory[] = {"Knife", "Rope", "Torch"};
+    int totalItems = 3;
+
+    if (index >= 0 && index < totalItems) {
+        return inventory[index];
+    } else {
+        return "Invalid item";
+    }
+}
+
+int lose_health(int health, int loss)
+{
+    health -= loss;
+    char option_restart;
+    if (health <= 0){
+        printf("You have died. Would you like to restart? ");
+        scanf("%c",&option_restart);
+    }
+    else{
+        printf("You have %c health", &health);
+    }
+    return health;
+} 
+
 
 int starting_room()
 {
@@ -17,32 +42,11 @@ int first_room()
 {
     printf("You enter a dimly lit room, you see a large chasm with a beam above it, what would you like to do? ");
     
-
 }
 
 
-
-
-
-int lose_health(int health, int loss)
-{
-    health -= loss;
-    char option_restart;
-    if (health <= 0){
-        printf("You have died. Would you like to restart? ");
-        scanf("%c",&option_restart);
-    }
-    else{
-        printf("You have %c health", &health);
-    }
-    return health;
-} 
-
 int main(){
     if (starting_room()) == "yes"{
-        first_room()
-
-    
-    
+        first_room()        
 }
 
